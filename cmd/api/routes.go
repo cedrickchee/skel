73ab8100671f5acd217d47df6e2795a3f1243691
Cedric Chee <cedric@invictusbyte.com>
2021-08-22 23:30:42 +0800

    router: fix routing errors
    The error messages that `httprouter` automatically sends when it can’t
    find a matching route by default, these will still be the same
    plain-text (non-JSON).
    
    Set our own custom error handlers when we initialize the router.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,16 @@ func (app *application) routes() *httprouter.Router {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler
+	// for 404 Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler
+	// and set it as the custom error handler for 405 Method Not Allowed
+	// responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL patterns and handler functions for our
 	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
 	// http.MethodPost are constants which equate to the strings 'GET' and
