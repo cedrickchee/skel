7be3a3ad5b045df7ac68d5116811b48c59e48ed1
Cedric Chee <cedric@invictusbyte.com>
2021-08-22 21:22:53 +0800

    customize JSON responses
    Customize the Runtime field of the Movie struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,11 +5,15 @@ import "time"
 // Annotate the Movie struct with struct tags to control how the keys appear in
 // the JSON-encoded output.
 type Movie struct {
-	ID        int64     `json:"id"`                // Unique integer ID for the movie
-	CreatedAt time.Time `json:"-"`                 // Timestamp for when the movie is added to our database
-	Title     string    `json:"title"`             // Movie title
-	Year      int32     `json:"year,omitempty"`    // Movie release year
-	Runtime   int32     `json:"runtime,omitempty"` // Movie runtime (in minutes)
-	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
-	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each time the movie information is updated
+	ID        int64     `json:"id"`             // Unique integer ID for the movie
+	CreatedAt time.Time `json:"-"`              // Timestamp for when the movie is added to our database
+	Title     string    `json:"title"`          // Movie title
+	Year      int32     `json:"year,omitempty"` // Movie release year
+	// Use the Runtime type instead of int32. Note that the omitempty directive
+	// will still work on this: if the Runtime field has the underlying value 0,
+	// then it will be considered empty and omitted -- and the MarshalJSON()
+	// method we just made won't be called at all.
+	Runtime Runtime  `json:"runtime,omitempty"` // Movie runtime (in minutes)
+	Genres  []string `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
+	Version int32    `json:"version"`           // The version number starts at 1 and will be incremented each time the movie information is updated
 }
