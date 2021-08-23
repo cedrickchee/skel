64900421b79a062856735710d3ec61ecb58c0c64
Cedric Chee <cedric@invictusbyte.com>
2021-08-23 22:03:23 +0800

    validate JSON input
    * Create a validator package.
    * Define a custom Validator type which contains a map of errors.
    * Perform validation checks.
    * Make validation rules reusable.

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -63,3 +63,10 @@ func (app *application) badRequestResponse(w http.ResponseWriter,
 	r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
+
+// Note that the errors parameter here has the type map[string]string, which is
+// exactly the same as the errors map contained in our Validator type.
+func (app *application) failedValidationResponse(w http.ResponseWriter,
+	r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
