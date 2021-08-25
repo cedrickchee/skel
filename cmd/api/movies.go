121717ae4aa445211d6a3c5c33dfcefa325c303f
Cedric Chee <cedric@invictusbyte.com>
2021-08-25 22:29:44 +0800

    create a new movie
    * Executing the SQL query.
    * Hook up the Insert() method to createMovieHandler so that
    POST /v1/movies endpoint works in full.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -36,6 +36,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	// Copy the values from the input struct to a new Movie struct.
+	// Note that the movie variable contains a *pointer* to a Movie struct.
 	movie := &data.Movie{
 		Title:   input.Title,
 		Year:    input.Year,
@@ -53,8 +54,29 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	// Dump the contents of the input struct in a HTTP response.
-	fmt.Fprintf(w, "%+v\n", input)
+	// Call the Insert() method on our movies model, passing in a pointer to the
+	// validated movie struct. This will create a record in the database and
+	// update the movie struct with the system-generated information.
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// When sending a HTTP response, we want to include a Location header to let
+	// the client know which URL they can find the newly-created resource at. We
+	// make an empty http.Header map and then use the Set() method to add a new
+	// Location header, interpolating the system-generated ID for our new movie
+	// in the URL.
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	// Write a JSON response with a 201 Created status code, the movie data in
+	// the response body, and the Location header.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // Add a showMovieHandler for the 'GET /v1/movies/:id' endpoint. For now, we
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/cedrickchee/skel/internal/validator"
+	"github.com/lib/pq"
 )
 
 // Annotate the Movie struct with struct tags to control how the keys appear in
@@ -53,9 +54,27 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-// Add a placeholder method for inserting a new record in the movies table.
+// The Insert() method accepts a pointer to a movie struct, which should contain
+// the data for the new record.
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	// Define the SQL query for inserting a new record in the movies table and
+	// returning the system-generated data.
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+
+	// Create an args slice containing the values for the placeholder parameters
+	// from the movie struct. Declaring this slice immediately next to our SQL
+	// query helps to make it nice and clear *what values are being used where*
+	// in the query.
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	// Use the QueryRow() method to execute the SQL query on our connection
+	// pool, passing in the args slice as a variadic parameter and scanning the
+	// system-generated id, created_at and version values into the movie
+	// struct.
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // Add a placeholder method for fetching a specific record from the movies
