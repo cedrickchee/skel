29cdd0a00e0b623c3c62fe46b1bc6444397902e1
Cedric Chee <cedric@invictusbyte.com>
2021-08-25 23:09:22 +0800

    fetch & display the data for a specific movie
    Update the showMovieHandler so that it calls the Get() method we just
    made.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/cedrickchee/skel/internal/data"
 	"github.com/cedrickchee/skel/internal/validator"
@@ -91,13 +91,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	// Call the Get() method to fetch the data for a specific movie. We also
+	// need to use the errors.Is() function to check if it returns a
+	// data.ErrRecordNotFound error, in which case we send a 404 Not Found
+	// response to the client.
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	// Encode the struct to JSON and send it as the HTTP response.
