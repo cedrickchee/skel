990079185c2fa054f0dde75c012fc670dfad0085
Cedric Chee <cedric@invictusbyte.com>
2021-08-27 12:12:04 +0800

    parse query string parameters
    * Create helper functions
    * Add the API handler & route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -25,6 +25,7 @@ func (app *application) routes() *httprouter.Router {
 	// http.MethodPost are constants which equate to the strings 'GET' and
 	// 'POST' respectively.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMovieHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
