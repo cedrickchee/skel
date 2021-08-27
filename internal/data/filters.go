d806aa8034da8af3433aad3c69d565f98a756300
Cedric Chee <cedric@invictusbyte.com>
2021-08-28 00:16:20 +0800

    implement sorting
    Update the logic for the endpoint so that the client can control how
    the movies are sorted in the JSON response.

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,39 @@
 package data
 
-import "github.com/cedrickchee/skel/internal/validator"
+import (
+	"strings"
+
+	"github.com/cedrickchee/skel/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string // hold the supported sort values
+}
+
+// Check that the client-provided Sort field matches one of the entries in our
+// safelist and if it does, extract the column name from the Sort field by
+// stripping the leading hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// Return the sort direction ('ASC' or 'DESC') depending on the prefix character
+// of the Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	} else {
+		return "ASC"
+	}
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/cedrickchee/skel/internal/validator"
@@ -231,12 +232,14 @@ func (m MovieModel) GetAll(title string, genres []string,
 	filters Filters) ([]*Movie, error) {
 	// Construct the SQL query to retrieve all movie records.
 	// Use full-text search for the title filter.
-	query := `
+	// Notice that we also include a secondary sort on the movie ID to ensure a
+	// consistent ordering.
+	query := fmt.Sprintf(`
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
         WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id`
+		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	// Create a context with a 3-second timeout.
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
