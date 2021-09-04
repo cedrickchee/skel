02d7c712fe7ea85cb0a066d3c4db7830b16ea0f3
Cedric Chee <cedric@invictusbyte.com>
2021-09-04 16:09:52 +0800

    create a request-level metrics middleware
    * The middleware record custom request-level metrics. It records the
    following three things:
        * The total number of requests received.
        * The total number of responses sent.
        * The total (cumulative) time taken to process all requests in microseconds.
    * Update routes to include the new middleware, right at the beginning
    of the chain.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"expvar"
 	"fmt"
 	"net"
 	"net/http"
@@ -392,5 +393,36 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 
 		// Call the next handler in the chain.
 		next.ServeHTTP(w, r)
+	})
+}
+
+// metrics records custom request-level metrics for our application.
+func (app *application) metrics(next http.Handler) http.Handler {
+	// Initialize the new expvar variables when the middleware chain is first
+	// built.
+	totalRequestsReceived := expvar.NewInt("total_requests_received")
+	totalResponsesSent := expvar.NewInt("total_responses_sent")
+	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_μs")
+
+	// The following code will be run for every request.
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Record the time that we started to process the request.
+		start := time.Now()
+
+		// Use the Add() method to increment the number of requests received by
+		// 1.
+		totalRequestsReceived.Add(1)
+
+		// Call the next handler in the chain.
+		next.ServeHTTP(w, r)
+
+		// On the way back up the middleware chain, increment the number of
+		// responses sent by 1.
+		totalResponsesSent.Add(1)
+
+		// Calculate the number of microseconds since we began to process the
+		// request, then increment the total processing time by this amount.
+		duration := time.Now().Sub(start).Microseconds()
+		totalProcessingTimeMicroseconds.Add(duration)
 	})
 }
