311f0055238257803559f771bc7a7f499df123ea
Cedric Chee <cedric@invictusbyte.com>
2021-09-15 18:15:20 +0800

    test: add test helpers
    Break out some of this code into some helper functions, which we
    can reuse as we add more end-to-end tests to the project.
    
    Changes:
    * Put the helpers in `cmd/api/testutils_test.go` file.
    * Update `TestHealthcheckHandler` to use the new helpers.

--- cmd/api/healthcheck_test.go
+++ cmd/api/healthcheck_test.go
@@ -2,58 +2,38 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/cedrickchee/skel/internal/jsonlog"
 )
 
 func TestHealthcheckHandler(t *testing.T) {
 	// ***** END-TO-END TESTING *****
 
-	// Create a new instance of our application struct. For now, this just
-	// contains a mock logger (which discard anything written to them).
-	app := &application{
-		config: config{
-			env: "test",
-		},
-		logger: jsonlog.New(io.Discard, jsonlog.LevelInfo),
-	}
+	app := newTestApplication(t)
 
-	// We then use the httptest.NewServer() function to create a new test
-	// server, passing in the value returned by our app.routes() method as the
-	// handler for the server. This starts up a HTTP server which listens on a
-	// randomly-chosen port of your local machine for the duration of the test.
-	// Notice that we defer a call to ts.Close() to shutdown the server when the
-	// test finishes.
-	//
+	// Spin up a test server and make a GET request to it.
+
 	// Doing this gives us a test server that exactly mimics our application
 	// routes, middleware and handlers, and is a big upside of the work that we
 	// did earlier to isolate all our application routing in the app.routes()
 	// method.
-	ts := httptest.NewServer(app.routes())
+	//
+	// Notice that we defer a call to ts.Close() to shutdown the server when the
+	// test finishes.
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	// The network address that the test server is listening on is contained in
-	// the ts.URL field. We can use this along with the ts.Client().Get() method
-	// to make a GET /healthcheck request against the test server. This returns
-	// a http.Response struct containing the response.
-	rs, err := ts.Client().Get(ts.URL + "/v1/healthcheck")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/v1/healthcheck")
 
 	// We can then check the value of the response status code and body.
-	if rs.StatusCode != http.StatusOK {
-		t.Fatalf("want %d; got %d", http.StatusOK, rs.StatusCode)
+	if code != http.StatusOK {
+		t.Fatalf("want %d; got %d", http.StatusOK, code)
 	}
 	// JSON decoding
 	var got envelope
-	err = json.NewDecoder(rs.Body).Decode(&got)
+	err := json.NewDecoder(body).Decode(&got)
 	if err != nil {
-		t.Fatalf("unable to parse response from server %q into envelope, '%v'", rs.Body, err)
+		t.Fatalf("unable to parse response from server %q into envelope, '%v'", body, err)
 	}
 	env := got["system_info"].(map[string]interface{})["environment"]
 	if env != "test" {
--- cmd/api/testutils_test.go
+++ cmd/api/testutils_test.go
@@ -1,10 +1,13 @@
 //lint:file-ignore U1000 WIP
+// Test helpers
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/cedrickchee/skel/internal/data"
@@ -15,7 +18,12 @@ import (
 // application struct containing mocked dependencies.
 func newTestApplication(t *testing.T) *application {
 	logger := jsonlog.New(ioutil.Discard, jsonlog.LevelInfo)
+	// For now, this just contains a mock logger (which discard anything written
+	// to them) and some mock models (only movie model).
 	return &application{
+		config: config{
+			env: "test",
+		},
 		logger: logger,
 		models: data.NewMockModels(),
 	}
@@ -28,6 +36,38 @@ type testServer struct {
 }
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
-	ts := httptest.NewTLSServer(h)
+	// Use the httptest.NewServer() function to create a new test server,
+	// passing in the handler for the server. This starts up a HTTP server which
+	// listens on a randomly-chosen port of your local machine for the duration
+	// of the test.
+	ts := httptest.NewServer(h)
 	return &testServer{ts}
+}
+
+// get method makes a GET request to a given url path on the test server, and
+// returns the response status code, headers and body.
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, io.ReadCloser) {
+	// The network address that the test server is listening on is contained in
+	// the ts.URL field. We can use this along with the ts.Client().Get() method
+	// to make a GET request against the test server. This returns a
+	// http.Response struct containing the response.
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// defer rs.Body.Close()
+	// body, err := ioutil.ReadAll(rs.Body)
+	// if err != nil {
+	//     t.Fatal(err)
+	// }
+
+	return rs.StatusCode, rs.Header, rs.Body
+}
+
+func assertEqual(t *testing.T, a, b interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(a, b) {
+		t.Fatalf("expecting values to be equal but got: '%v' and '%v'", a, b)
+	}
 }
